bdbd9ec7f807362981456c0910fa38fa2f4e600d
Renato Fialho <renato@renatofialho.com>
2014-09-13 01:42:35 -0300

    Teste

--- JantardosCanibais/src/org/Cannibal.java
+++ JantardosCanibais/src/org/Cannibal.java
@@ -15,7 +15,6 @@ public class Cannibal extends Thread {
 			try {
 				Thread.sleep(50);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
